fc6daa7f7892b2e2d355e49a281de467f9c14182
HoussaineBenDhieb <131673785+houssainebendhieb@users.noreply.github.com>
2024-05-07 08:54:11 +0100

    initProject

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: Container(),
+  ));
+}
